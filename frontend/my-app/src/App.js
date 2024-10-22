290e1fac314722e9cfe2c09e484933bf2d8731e7
TrusTa01 <danylnet65@gmail.com>
2024-10-23 06:08:44 +1000

    🔍

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -13,7 +13,6 @@ function App() {
   return (
     <>
     <HomePage />
-    <Test />
     <Sidebar />
     </>
   );
--- frontend/my-app/src/components/Sidebar.jsx
+++ frontend/my-app/src/components/Sidebar.jsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import './Sidebar.css';
 
-const Sidebar = () => {
+const Sidebar = () => {  {/*заменить картики на иконки*/}
   return (
     <div className="sidebar">
-       <a href="https://example.com" rel="" >
-       <img src="./images/logo.png style={{ width: '100%', height: 'auto' }}" alt="" />
-       </a>
-      <span>Главная VL.ru</span>
-    </div>
+      <div className='container'>
+      <div className='line'></div>
+      <a href="https://example.com" rel="">
+        <img src="./images/logo2.png" className='logo2' alt="Logo"/>
+        <span className='home'> Главная VL.ru</span>
+      </a>
+      <div className='line'></div>
+
+      <div className='login'>
+        <a href="https://example.com">
+          <img src="./images/logo2.png" alt="User"/>
+          <span className='Fullname'>Райан Гослинг</span>
+        </a>
+      </div>
+
+      <div className='tickets'>
+        <a href="https://example.com">
+          <img src="./images/logo2.png" alt="Tickets"/>
+          <span className='tickets-vlru'>Билеты VL.ru</span>
+        </a>
+      </div>
+
+      <div className='selected'>
+        <a href="https://example.com">
+          <img src="./images/logo2.png" alt="Favorites"/>
+          <span className='favourite'>Избранное</span>
+        </a>
+        <div className='line2'></div>
+      </div>
+
+    <div className='location'>
+    <a href="https://example.com">
+      <img src="./images/logo2.png" alt="Сity"/>
+      <span className='city'>Владивосток</span>
+    </a>
+    <div className='line2'></div>
+  </div>
+
+  <div className='log-out'>
+    <a href="https://example.com">
+      <img src="./images/logo2.png" alt="Выход"/>
+      <span className='logout'>Владивосток</span>
+    </a>
+    <div className='line2'></div>
+  </div>
+  </div>
+</div>
   );
 };
 
 export default Sidebar;
-
-
