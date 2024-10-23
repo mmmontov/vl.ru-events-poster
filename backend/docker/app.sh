868ffbb0b144e824a189b07c267c02f74f46d0af
Mikhail Mamontov <mmontovmm@gmail.com>
2024-10-23 23:04:39 +1000

    create docker

---
+++ backend/docker/app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+alembic revision --autogenerate -m "create"
+
+alembic upgrade head
+
+gunicorn src.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+ 
